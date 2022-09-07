f8411a141b851f2a93f0ba81b47c4f5de1b826f9
sean <seanhsu0102@gmail.com>
2022-09-07 16:10:26 +0800

    Upgrade pyOpenSSL

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    install_requires=["pycryptodomex==3.9.4", "pyOpenSSL==20.0.0"],
+    install_requires=["pycryptodomex==3.9.4", "pyOpenSSL==22.0.0"],
     test_suite="setup.alipay_test_suite"
 )
